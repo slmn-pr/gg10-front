8d8f327464874a2d41175085ab7e1b0caf4a531b
slmn-pr <soleimanpoursalman@gmail.com>
2026-01-01 00:38:14 +0330

    Enhance OtpVerificationSection layout and functionality
    Refactor OtpVerificationSection to improve the user interface by adding a stepper for OTP verification progress, a logo, and a countdown timer. Introduce a button for changing the phone number and update the overall layout for better user experience. This change aligns with the new authentication flow and enhances the visual design of the OTP verification process.

--- src/pages/auth/containers/OtpVerificationSection.jsx
+++ src/pages/auth/containers/OtpVerificationSection.jsx
@@ -1,9 +1,111 @@
-import { Box, Typography } from '@mui/material';
+import CloseIcon from '@/components/icons/general/CloseIcon';
+import Logo from '@/components/icons/Logo';
+import BackwardButton from '@/components/layout/BackwardButton';
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Stack,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
 
-export default function OtpVerificationSection() {
+export default function OtpVerificationSection({ phoneNumber = '09123456789' }) {
+  const [activeStep, setActiveStep] = useState(0);
+
   return (
-    <Box>
-      <Typography variant="title1">تایید و دریافت کد</Typography>
-    </Box>
+    <Container
+      maxWidth="sm"
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: 'background.default',
+        px: { xs: '16px' },
+        pt: { xs: '16px' },
+      }}
+    >
+      {/* Header */}
+      <Box
+        sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+      >
+        <IconButton>
+          <CloseIcon />
+        </IconButton>
+
+        <BackwardButton>ساخت حساب کاربری</BackwardButton>
+      </Box>
+
+      {/* Step progress */}
+      <Stepper activeStep={activeStep} alternativeLabel sx={{ width: '100%' }}>
+        <Step>
+          <StepLabel>
+            {/* <Typography variant="title1">تایید و دریافت کد</Typography> */}
+          </StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>
+            {/* <Typography variant="title1">تایید و دریافت کد</Typography> */}
+          </StepLabel>
+        </Step>
+
+        <Step>
+          <StepLabel>
+            {/* <Typography variant="title1">تایید و دریافت کد</Typography> */}
+          </StepLabel>
+        </Step>
+      </Stepper>
+
+      {/* LOgo */}
+      <Box sx={{ mt: '100px', display: 'flex', justifyContent: 'center' }}>
+        <img src="/images/logo.png" alt="logo" style={{ height: '38px' }} />
+      </Box>
+
+      {/* Otp section */}
+      <Stack>
+        <Typography component="p" variant="title3">
+          کد تایید را وارد کنید
+        </Typography>
+
+        {/* Change number  */}
+        <Box
+          sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+        >
+          <Button variant="text" color="primary">
+            <Typography variant="sub1" component="p" color="primary.main">
+              تغییر شماره
+            </Typography>
+          </Button>
+
+          <Typography variant="sub1" component="p">
+            کد 5 رقمی به شماره {phoneNumber} ارسال شد
+          </Typography>
+        </Box>
+
+        {/* Otp input */}
+        <Box>{/* <OtpInput /> */}</Box>
+
+        {/* Countdown */}
+        <Box>
+          <Typography variant="sub3" component="p" color="custom.grey0">
+            01:30 تا درخواست مجدد ارسال کد
+          </Typography>
+        </Box>
+
+        {/* Button */}
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ width: '252px', mx: 'auto', mt: '100px' }}
+          disabled={true}
+        >
+          <Typography variant="button1" component="p" color="white">
+            تایید و ادامه
+          </Typography>
+        </Button>
+      </Stack>
+    </Container>
   );
 }
